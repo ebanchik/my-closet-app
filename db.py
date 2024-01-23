b2bf024cc2c6c86e9ba174c16e94972b649005b4
ebanchik <ebanchik@gmail.com>
2024-01-23 12:39:56 -0500

    add database with tables

---
+++ db.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+
+def connect_to_db():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def initial_setup():
+    conn = connect_to_db()
+    conn.execute(
+        """
+        DROP TABLE IF EXISTS items;
+        """
+    )
+    conn.execute(
+        """
+        CREATE TABLE items (
+          id INTEGER PRIMARY KEY NOT NULL,
+          name TEXT,
+          brand TEXT,
+          size INT,
+          color TEXT,
+          fit TEXT,
+          category_id INT
+        );
+        """
+    )
+    conn.commit()
+    print("Table created successfully")
+
+    items_seed_data = [
+        ("Loose-fit Jeans", "HOPE", 34, "Mid Grey Stone", "very baggy", 2),
+    ]
+    conn.executemany(
+        """
+        INSERT INTO items (name, brand, size, color, fit, category_id)
+        VALUES (?,?,?,?,?,?)
+        """,
+        items_seed_data,
+    )
+    conn.commit()
+    print("Seed data created successfully")
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    initial_setup()
